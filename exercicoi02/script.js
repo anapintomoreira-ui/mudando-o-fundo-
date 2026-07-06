bec316621f3e7aafaed7f87e6592d2c55738922a
anapintomoreira-ui <ana.pinto.moreira@escola.pr.gov.br>
2026-07-06 12:29:30 -0300

    Implement light and dark mode functionality
    Fixes background color setting and adds messages for light and dark modes.

--- exercicoi02/script.js
+++ exercicoi02/script.js
@@ -2,5 +2,15 @@ function modoClaro () {
     document.body.style.backgroundColor = "white";
     document.body.style.color = "black";
 
-    
-}
+
+document.getElementById("mensagem").innerText = "Modo Claro Ativado!";
+
+    {
+
+
+    function modoEscuro() {
+        document.body.tyle.backgrounfColor = "black";
+        document.body.style.color = "white";
+
+
+        document.getElementById("mensagem").innerText = "Modo escuro ativado!";
